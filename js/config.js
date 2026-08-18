9559b5e973eb60f5700dca32c5b7ce9406ec73d6
watersplashfestau <damianmaheswara@gmail.com>
2026-08-18 10:37:55 +0700

    Initial website

---
+++ js/config.js
@@ -0,0 +1,24 @@
+window.JENIFFER_CONFIG = {
+    sheets: {
+        albums:
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vRxtXpCT9-SV_V0qATfxnHWrVGzGxaKqpE2wC0sDokvtxF7BYtGylaV2yVEdkyKKTUDEiSllaZNrN-u/pub?gid=1317380980&single=true&output=csv",
+
+        koreanAlbums:
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vRxtXpCT9-SV_V0qATfxnHWrVGzGxaKqpE2wC0sDokvtxF7BYtGylaV2yVEdkyKKTUDEiSllaZNrN-u/pub?gid=1583281469&single=true&output=csv",
+
+        singles:
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vRxtXpCT9-SV_V0qATfxnHWrVGzGxaKqpE2wC0sDokvtxF7BYtGylaV2yVEdkyKKTUDEiSllaZNrN-u/pub?gid=1064742388&single=true&output=csv",
+
+        videos:
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vRxtXpCT9-SV_V0qATfxnHWrVGzGxaKqpE2wC0sDokvtxF7BYtGylaV2yVEdkyKKTUDEiSllaZNrN-u/pub?gid=328895011&single=true&output=csv",
+
+        tour:
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vRxtXpCT9-SV_V0qATfxnHWrVGzGxaKqpE2wC0sDokvtxF7BYtGylaV2yVEdkyKKTUDEiSllaZNrN-u/pub?gid=1480274934&single=true&output=csv",
+
+        shop:
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vRxtXpCT9-SV_V0qATfxnHWrVGzGxaKqpE2wC0sDokvtxF7BYtGylaV2yVEdkyKKTUDEiSllaZNrN-u/pub?gid=724586775&single=true&output=csv",
+
+        settings:
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vRxtXpCT9-SV_V0qATfxnHWrVGzGxaKqpE2wC0sDokvtxF7BYtGylaV2yVEdkyKKTUDEiSllaZNrN-u/pub?gid=1864435122&single=true&output=csv"
+    }
+};
